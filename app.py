b94c3e9ac450547600114d12053b26995482e15f
Prita Trivena Hesmono <prita.trivena.h@gmail.com>
2024-12-20 06:49:11 +0700

    yea

--- app.py
+++ app.py
@@ -32,20 +32,15 @@ if uploaded_file is not None:
     # Kolom yang digunakan untuk prediksi
     selected_columns = [
         # Kolom untuk layanan_balita
-        'Bayi 0-11 Bulan Imunisasi Lengkap',
-        'Anak 6-59 Bulan Dapat Vit A',
-        'Balita Dapat Zinc',
-        'Pendek (anak)',
-        'Sangat Pendek (anak)',
+        'Pendek (anak)', 'Sangat Pendek (anak)','Balita Kurus dapat PMT','Bayi 0-11 Bulan Imunisasi Lengkap','Anak Ikut PAUD','Balita Dapat Zinc',
 
         # Kolom untuk layanan_ibu_hamil
-        'Bumil KEK dapat PMT',
-        'Bumil dapat IFA',
-        'Bumil Konseling Gizi'
+        'Bumil Konseling Gizi',
+        'Keluarga Bina Balita',
 
         # Kolom untuk layanan_sosial_gizi
-        #'Kelompok Keluarga Penerima Manfaat (KPM) Program Keluarga Harapan (PKH) yang mengikuti Pertemuan Peningkatan Kemampuan Keluarga (P2K2) dengan modul kesehatan dan gizi',
-        #Keluarga Penerima Manfaat (KPM) dengan ibu hamil, ibu menyusui, dan baduta yang menerima variasi bantuan pangan selain beras dan telur'
+        'RT Air Minum Layak',
+        '	RT Sanitasi Layak'
     ]
 
     # Memastikan kolom yang dibutuhkan ada di DataFrame
